d4093d7a91c20c800784ca6a36a606d8a4b7734f
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-04-27 16:29:44 +0200

    Renamed actwa -> aw

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from setuptools import setup, find_packages
 
-setup(name='actwa-core',
+setup(name='aw-core',
       version='0.1',
       description='Core library for ActivityWatch',
       author='Erik Bjäreholt',
       author_email='erik@bjareho.lt',
-      url='https://github.com/ActivityWatch/actwa-core',
-      packages=['actwa.core'],
-      namespace_packages=['actwa']
+      url='https://github.com/ActivityWatch/aw-core',
+      packages=['aw.core'],
+      namespace_packages=['aw']
      )
