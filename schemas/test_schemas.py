27da5bcf1bba4954e28224b048abe9ef4cd8128f
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-05-21 23:01:58 +0200

    Fixed example schema

--- schemas/test_schemas.py
+++ schemas/test_schemas.py
@@ -1,34 +1,57 @@
 import json
 
-from jsonschema import validate
+from jsonschema import validate as _validate, FormatChecker
 from jsonschema.exceptions import ValidationError
 
 import unittest
+
+# TODO: Include date-time format
+# https://python-jsonschema.readthedocs.io/en/latest/validate/#jsonschema.FormatChecker
+
+# The default FormatChecker, uses the date-time checker
+fc = FormatChecker(["date-time"])
 
 class ExampleTest(unittest.TestCase):
     def setUp(self):
         self.schema = json.load(open("example.json"))
 
+    def validate(self, obj):
+        _validate(obj, self.schema, format_checker=fc)
+
     def test_timestamp(self):
-        validate({"timestamp": ["1937-01-01T12:00:27.87+00:20"]}, self.schema)
-        validate({"timestamp": ["1937-01-01T12:00:27.87+00:20"]*2}, self.schema)
+        self.validate({"timestamp": "1937-01-01T12:00:27.87+00:20"})
+        self.validate({"timestamp": "1937-01-01T12:00:27.87+00:20"})
+        self.validate({"timestamp": ["1937-01-01T12:00:27.87+00:20"]*2})
 
-    def test_timestamp_missing(self):
+    def test_timestamp_invalid_string(self):
         with self.assertRaises(ValidationError):
-            validate({"label": "test-label"}, self.schema)
+            self.validate({"timestamp": ""})
+        with self.assertRaises(ValidationError):
+            self.validate({"timestamp": "123"})
 
-    # FIXME: Validates when it shouldn't
-    # IMPORTANT: This means that no date-time validation happens within the array
-    #def test_timestamp_invalid_array(self):
-    #    with self.assertRaises(ValidationError):
-    #        validate({"timestamp": [""]}, self.schema)
+    def test_timestamp_invalid_array(self):
+        with self.assertRaises(ValidationError):
+            self.validate({"timestamp": [""]})
+        with self.assertRaises(ValidationError):
+            self.validate({"timestamp": ["123"]})
+
+    def test_timestamp_invalid_number(self):
+        with self.assertRaises(ValidationError):
+            self.validate({"timestamp": 2})
 
     def test_timestamp_empty_array(self):
         with self.assertRaises(ValidationError):
-            validate({"timestamp": []}, self.schema)
+            self.validate({"timestamp": []})
 
     def test_label(self):
-        validate({"label": "test-label", "timestamp": ["1937-01-01T12:00:27.87+00:20"]}, self.schema)
+        self.validate({"label": "test-label"})
+
+    def test_duration(self):
+        self.validate({"duration": {"value": 1000, "unit": "seconds"}})
+
+    def test_duration_invalid_unit(self):
+        with self.assertRaises(ValidationError):
+            self.validate({"duration": {"value": 1000, "unit": "rainbows"}})
 
 
 if __name__ == "__main__":
