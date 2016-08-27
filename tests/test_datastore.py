973aff3bd7f127e5034498d510a3496f97e2d561
Johan Bjäreholt <johanthepro@gmail.com>
2016-08-27 12:55:37 +0200

    Added duration assert to chunking test

--- tests/test_datastore.py
+++ tests/test_datastore.py
@@ -100,13 +100,15 @@ def test_chunking(bucket_cm):
         eventcount = 10
         events = []
         for i in range(eventcount):
-            events.append(Event(**{"label": "test", "timestamp": datetime.now(timezone.utc)+timedelta(seconds=i)}))
+            events.append(Event(**{"label": "test", "timestamp": datetime.now(timezone.utc)+timedelta(seconds=i), "duration": {"value": 1, "unit": "s"}}))
         bucket.insert(events)
         # Assert
         res = bucket.chunk()
         print(res)
         assert_equal(res['eventcount'], eventcount)
         assert_equal(res['chunks']['test']['other_labels'], [])
+        assert_equal(res['chunks']['test']['duration'], {"value": 10, "unit": "s"})
+        print(a)
 
 
 @parameterized(param_testing_buckets_cm())
