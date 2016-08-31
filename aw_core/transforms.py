49523c192e90349fa8828d875fa0e512164afb5f
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-08-31 18:02:40 +0200

    First version of filter_period_intersect, not extensively tested

--- aw_core/transforms.py
+++ aw_core/transforms.py
@@ -8,44 +8,61 @@ from aw_core import TimePeriod
 logger = logging.getLogger("aw.core.transform")
 
 
-def get_event_interval(event: Event) -> TimePeriod:
+def _get_event_period(event: Event) -> TimePeriod:
+    # TODO: Better parsing of event duration
     start = event["timestamp"][0]
-    end = start + event["duration"][0]
+    end = start + timedelta(seconds=event["duration"][0]["value"])
     return TimePeriod(start, end)
 
 
-def filter_afk_events(events, afkevents):
-    # TODO: Generalize for filtering with other events than afkevents
-    # https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
-    afkevents = sorted(afkevents, key=lambda e: e["timestamp"][0])
+def _replace_event_period(event: Event, period: TimePeriod) -> Event:
+    e = event.copy()
+    e["timestamp"] = [period.start]
+    e["duration"] = [period.duration]
+    return e
+
+
+def filter_period_intersect(events, filterevents):
+    """
+    Filters away all events or time periods of events in which a
+    filterevent does not have an intersecting time period.
+
+    Useful for example when you want to filter away events or
+    part of events during which a user was AFK.
+
+    Example:
+      windowevents_notafk = filter_period_intersect(windowevents, notafkevents)
+    """
+
+    events = sorted(events, key=lambda e: e["timestamp"][0])
+    filterevents = sorted(filterevents, key=lambda e: e["timestamp"][0])
     filtered_events = []
 
     e_i = 0
-    a_i = 0
-    while e_i < len(events) and a_i < len(afkevents):
+    f_i = 0
+    while e_i < len(events) and f_i < len(filterevents):
         event = events[e_i]
-        afkevent = afkevents[a_i]
-        e_interval = get_event_interval(event)
-        a_interval = get_event_interval(afkevent)
+        filterevent = filterevents[f_i]
+        ep = _get_event_period(event)
+        fp = _get_event_period(filterevent)
 
-        if e_interval.is_within(a_interval):
-            filtered_events.append(event)
+        ip = ep.intersection(fp)
+        if ip:
+            # If events itersected, add event with intersected duration and try next event
+            filtered_events.append(_replace_event_period(event, ip))
+            e_i += 1
+        else:
+            # No intersection, check if event is before/after filterevent
+            if ep.end < fp.start:
+                # Event ended before filter event started
+                e_i += 1
+            elif fp.end < ep.start:
+                # Event started after filter event ended
+                f_i += 1
+            else:
+                raise Exception("Should be unreachable")
 
-        raise NotImplementedError
-
-
-"""
-def test_filter_afk_events():
-    # Entire event overlaps
-    events = [Event(timestamp=datetime.now(), duration=timedelta(hours=1))]
-    afkevents = [Event(timestamp=datetime.now() + timedelta(minutes=10), duration=timedelta(minutes=30))]
-    print(filter_afk_events(events, afkevents))
-
-    # Event doesn't overlap
-    events = [Event(timestamp=datetime.now(), duration=timedelta(hours=1))]
-    afkevents = [Event(timestamp=datetime.now() + timedelta(hours=2), duration=timedelta(hours=1))]
-    print(filter_afk_events(events, afkevents))
-"""
+    return filtered_events
 
 
 def chunk(events: List[Event]) -> dict:
--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -5,26 +5,46 @@ from datetime import datetime, timedelta, timezone
 from nose.tools import assert_equal, assert_dict_equal, assert_raises
 
 from aw_core.models import Event
-from aw_core.transforms import chunk, filter_afk_events
+from aw_core.transforms import chunk, filter_period_intersect
 
 
-def test_chunk():
+class ChunkTest(unittest.TestCase):
     # Tests the chunk transform
-    eventcount = 10
-    events = []
-    for i in range(eventcount):
-        events.append(Event(label=["test", "test2"],
-                            timestamp=datetime.now(timezone.utc) + timedelta(seconds=i),
-                            duration={"value": 1, "unit": "s"}))
-    res = chunk(events)
-    logging.debug(res)
-    assert_equal(res['eventcount'], eventcount)
-    assert_equal(res['chunks']['test']['other_labels'], ["test2"])
-    assert_equal(res['chunks']['test']['duration'], {"value": eventcount, "unit": "s"})
-    assert_equal(res['chunks']['test2']['other_labels'], ["test"])
-    assert_equal(res['chunks']['test2']['duration'], {"value": eventcount, "unit": "s"})
+
+    def test_chunk(self):
+        eventcount = 10
+        events = []
+        for i in range(eventcount):
+            events.append(Event(label=["test", "test2"],
+                                timestamp=datetime.now(timezone.utc) + timedelta(seconds=i),
+                                duration={"value": 1, "unit": "s"}))
+        res = chunk(events)
+        logging.debug(res)
+        assert_equal(res['eventcount'], eventcount)
+        assert_equal(res['chunks']['test']['other_labels'], ["test2"])
+        assert_equal(res['chunks']['test']['duration'], {"value": eventcount, "unit": "s"})
+        assert_equal(res['chunks']['test2']['other_labels'], ["test"])
+        assert_equal(res['chunks']['test2']['duration'], {"value": eventcount, "unit": "s"})
 
 
-@unittest.skip
-def test_filter_afk_events():
-    filter_afk_events([])
+class FilterPeriodIntersectTest(unittest.TestCase):
+    def test_filter_period_intersect(self):
+        td1h = timedelta(hours=1)
+        td30min = timedelta(minutes=30)
+        now = datetime.now()
+
+        # Filter 1h event with another 1h event at a 30min offset
+        to_filter = [Event(label="lala", timestamp=now, duration=td1h)]
+        filter_with = [Event(timestamp=now + timedelta(minutes=30), duration=td1h)]
+        filtered_events = filter_period_intersect(to_filter, filter_with)
+        assert_equal(filtered_events[0]["duration"][0], timedelta(minutes=30))
+
+        # Filter 2x 30min events with a 15min gap with another 45min event in between intersecting both
+        to_filter = [
+            Event(label="lala", timestamp=now, duration=td30min),
+            Event(label="lala", timestamp=now + timedelta(minutes=45), duration=td30min)
+        ]
+        filter_with = [Event(timestamp=now + timedelta(minutes=15), duration=timedelta(minutes=45))]
+        filtered_events = filter_period_intersect(to_filter, filter_with)
+        assert_equal(filtered_events[0]["duration"][0], timedelta(minutes=15))
+        assert_equal(filtered_events[1]["duration"][0], timedelta(minutes=15))
