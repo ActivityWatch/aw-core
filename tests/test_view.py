a0012c7992a244bb6c571ab1e343609c80a69ad4
Johan Bjäreholt <johan@bjareho.lt>
2017-04-02 19:38:36 +0200

    Improved view tests

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,18 +1,68 @@
 from datetime import datetime, timedelta, timezone
 
 import pytest
+from pytest import raises
 
 from .utils import param_datastore_objects
 
 from aw_core.models import Event
-from aw_core.views import create_view, query_view, query_multiview, get_view, get_views
+from aw_core.views import create_view, query_view, query_multiview, get_view, get_views, ViewException
 
+bid1 = "bucket1"
+bid2 = "bucket2"
+example_view = {
+    'name': 'exview',
+    'created': '2016-09-03',
+    'query': {
+        'chunk': 'full',
+        'cache': True,
+        'transforms': [
+            {
+                'bucket': bid1,
+                'filters': [{
+                    'name': 'include_labels',
+                    'labels': ['test1'],
+                }],
+            }, {
+                'bucket': bid2,
+                'filters': [{
+                    'name': 'exclude_labels',
+                    'labels': ['test1'],
+                }],
+            }
+        ]
+    }
+}
+
+def test_get_view():
+    assert None == get_view("exview")
+    create_view(example_view)
+    assert None != get_view("exview")
+
+@pytest.mark.parametrize("datastore", param_datastore_objects())
+def test_bad_query(datastore):
+    try:
+        name = "A test bucket"
+        bucket1 = datastore.create_bucket(bucket_id=bid1, type="test", client="test", hostname="test", name=name)
+        bucket2 = datastore.create_bucket(bucket_id=bid2, type="test", client="test", hostname="test", name=name)
+        # Check that it raises an exception when the view doesn't exist
+        with raises(ViewException):
+            query_view("nosuchview", datastore, [], [])
+        with raises(ViewException):
+            query_multiview("nosuchview", datastore, [], [])
+
+        create_view(example_view)
+
+        # Check that it raises an exception when there are more/less start points than end points
+        with raises(ViewException):
+            query_multiview("exview", datastore, [1], [])
+    finally:
+        datastore.delete_bucket(bid1)
+        datastore.delete_bucket(bid2)
 
 @pytest.mark.parametrize("datastore", param_datastore_objects())
 def test_view(datastore):
-    name = "A label/name for a test bucket"
-    bid1 = "bucket1"
-    bid2 = "bucket2"
+    name = "A test bucket"
     now = datetime.now(timezone.utc)
     try:
         bucket1 = datastore.create_bucket(bucket_id=bid1, type="test", client="test", hostname="test", name=name)
@@ -27,41 +77,22 @@ def test_view(datastore):
         bucket1.insert(5 * [e2])
         bucket2.insert(5 * [e1])
         bucket2.insert(10 * [e2])
-        example_view = {
-            'name': 'exview',
-            'created': '2016-09-03',
-            'query': {
-                'chunk': 'full',
-                'cache': True,
-                'transforms': [
-                    {
-                        'bucket': bid1,
-                        'filters': [{
-                            'name': 'include_labels',
-                            'labels': ['test1'],
-                        }],
-                    }, {
-                        'bucket': bid2,
-                        'filters': [{
-                            'name': 'exclude_labels',
-                            'labels': ['test1'],
-                        }],
-                    }
-                ]
-            }
-        }
         # Test creating view
         create_view(example_view)
-        assert get_view('exview') == example_view
-        assert get_views() == ['exview']
+        assert example_view == get_view('exview')
+        assert ['exview'] == get_views()
         # Test that output is correct
         result = query_view('exview', datastore)
         assert result['chunks']['test1'] == {'duration': {'value': 10.0, 'unit': 's'}, "keyvals": {}}
         assert result['chunks']['test2'] == {'duration': {'value': 20.0, 'unit': 's'}, "keyvals": {}}
         # Test that starttime works
         assert 10 == query_view('exview', datastore, start=now - timedelta(hours=1))["eventcount"]
+        # Test starttime cached which is past now isn't cached
+        assert 10 == query_view('exview', datastore, start=now - timedelta(hours=1))["eventcount"]
         # Test that endtime works
-        assert 10 == query_view('exview', datastore, end=now - timedelta(hours=1))["eventcount"]
+        assert 10 == query_view('exview', datastore, start=now-timedelta(hours=200), end=now - timedelta(hours=1))["eventcount"]
+        # Test endtime cached
+        assert 10 == query_view('exview', datastore, start=now-timedelta(hours=200), end=now - timedelta(hours=1))["eventcount"]
         # Test that multiquery works
         starts = [now-timedelta(hours=1), now-timedelta(hours=100)]
         ends = [now+timedelta(hours=1),now+timedelta(hours=1)]
