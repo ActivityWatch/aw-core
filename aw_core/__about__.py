1f78bc5717b6f36c5f23396c8d5486a5679597be
Erik Bjäreholt <erik@bjareho.lt>
2017-05-08 23:55:36 +0200

    migrated package metadata into __about__.py

---
+++ aw_core/__about__.py
@@ -0,0 +1,19 @@
+# Inspired by:
+# https://github.com/pypa/pipfile/blob/master/pipfile/__about__.py
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "aw-core"
+__summary__ = "Core library for ActivityWatch"
+__uri__ = "https://github.com/ActivityWatch/aw-core"
+
+__version__ = "0.3.0"
+
+__author__ = "Erik Bjäreholt, Johan Bjäreholt"
+__email__ = "erik@bjareho.lt, johan@bjareho.lt"
+
+__license__ = "MPL2"
+__copyright__ = "Copyright 2017 %s" % __author__
--- setup.py
+++ setup.py
@@ -1,13 +1,39 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+import os
+import re
+from typing import Any
 
-setup(name='aw-core',
-      version='0.3',
-      description='Core library for ActivityWatch',
-      author='Erik Bjäreholt, Johan Bjäreholt',
-      author_email='erik@bjareho.lt, johan@bjareho.lt',
-      url='https://github.com/ActivityWatch/aw-core',
+from setuptools import setup
+
+
+base_dir = os.path.dirname(__file__)
+
+about = {}  # type: Any
+with open(os.path.join(base_dir, "aw_core", "__about__.py")) as f:
+    exec(f.read(), about)
+
+with open(os.path.join(base_dir, "README.md")) as f:
+    long_description = f.read()
+
+if False:
+    with open(os.path.join(base_dir, "CHANGELOG.rst")) as f:
+        # Remove :issue:`ddd` tags that breaks the description rendering
+        changelog = re.sub(
+            r":issue:`(\d+)`",
+            r"`#\1 <https://github.com/pypa/pipfile/issues/\1>`__",
+            f.read(),
+        )
+        long_description = "\n".join([long_description, changelog])
+
+
+setup(name              =about["__title__"],
+      version           =about["__version__"],
+      description       =about["__summary__"],
+      long_description  =long_description,
+      author            =about["__author__"],
+      author_email      =about["__email__"],
+      url               =about["__uri__"],
       packages=set(["aw_core", "aw_datastore"]),
       install_requires=[
           'jsonschema',
