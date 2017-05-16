2741ab47532937217fbe9d975d9bd9d1709986ff
Erik Bjäreholt <erik@bjareho.lt>
2017-05-16 22:46:09 +0200

    improved typing

--- aw_core/query.py
+++ aw_core/query.py
@@ -1,5 +1,7 @@
 from typing import Union, List
 
+from aw_datastore import Datastore
+
 from .models import Event
 
 from . import transforms
@@ -12,44 +14,66 @@ class QueryException(Exception):
 
 
 class Query:
-    def __init__(self, transforms, chunk=False, cache=False):
+    def __init__(self, transforms: List[dict], chunk=False, cache=False) -> None:
         self.transforms = transforms
         self.chunk = chunk
         self.cache = cache
 
 
-def bucket_transform(btransform, ds, limit=-1, start=None, end=None):
-    if "bucket" not in btransform:
-        raise QueryException("No bucket specified in transform: {}".format(btransform))
-    if not isinstance(btransform["bucket"], str):
-        raise QueryException("Invalid bucket name in transform: '{}'".format(btransform["bucket"]))
-    if not btransform["bucket"] in ds.buckets():
-        raise QueryException("Cannot query bucket that doesn't exist in transform: '{}'".format(btransform["bucket"]))
+class BucketTransform:
+    def __init__(self, bucket_id: str, filters: dict) -> None:
+        self.bucket_id = bucket_id
+        self.filters = filters
+
+
+# TODO: Get rid of union here?
+def bucket_transform(btransform: Union[BucketTransform, dict], ds: Datastore,
+                     limit: int=-1, start: datetime=None, end: datetime=None):
+    if isinstance(btransform, dict):
+        # Run-time typechecks
+        if "bucket" not in btransform:
+            raise QueryException("No bucket specified in transform: {}".format(btransform))
+        if not isinstance(btransform["bucket"], str):
+            raise QueryException("Invalid bucket name in transform: '{}'".format(btransform["bucket"]))
+
+        # Fetch the bucket, ensure the bucket exists
+        bucket_id = btransform["bucket"]  # type: str
+        if not btransform["bucket"] in ds.buckets():
+            raise QueryException("Cannot query bucket that doesn't exist in transform: '{}'".format(btransform["bucket"]))
+
+        btransform = BucketTransform(bucket_id, filters=btransform["filters"] if "filters" in btransform else [])
+
     # Get events
-    events = ds[btransform["bucket"]].get(limit, start, end)
+    events = ds[btransform.bucket_id].get(limit, start, end)
+
     # Apply filters
-    if "filters" in btransform:
-        for vfilter in btransform["filters"]:
-            if "name" not in vfilter:
-                raise QueryException("No filter specified in transform: {}".format(vfilter))
-            filtername = vfilter["name"]
-            if not isinstance(filtername, str):
-                raise QueryException("Invalid filter name in transform: '{}'".format(filtername))
-            if filtername not in filters:
-                raise QueryException("No such filter in transform: '{}'".format(filtername))
-            events = filters[filtername](vfilter, events, ds, limit, start, end)
+    for vfilter in btransform.filters:
+        if "name" not in vfilter:
+            raise QueryException("No filter specified in transform: {}".format(vfilter))
+        filtername = vfilter["name"]
+        if not isinstance(filtername, str):
+            raise QueryException("Invalid filter name in transform: '{}'".format(filtername))
+        if filtername not in filters:
+            raise QueryException("No such filter in transform: '{}'".format(filtername))
+        events = filters[filtername](vfilter, events, ds, limit, start, end)
+
     return events
 
 
-def query(query: Union[Query, dict], ds, limit=-1, start=None, end=None):
-    events = []
-    if "transforms" not in query:
-        raise QueryException("Query does not contain a transform: {}".format(query))
-    for transform in query["transforms"]:
+# TODO: Get rid of union here?
+def query(query: Union[Query, dict], ds: Datastore,
+          limit: int=-1, start: datetime=None, end: datetime=None):
+    if isinstance(query, dict):
+        if "transforms" not in query:
+            raise QueryException("Query does not contain a transform: {}".format(query))
+        query = Query(query["transforms"], chunk=query["chunk"] if "chunk" in query else False)
+
+    events = []  # type: List[Event]
+    for transform in query.transforms:
         events += bucket_transform(transform, ds, limit, start, end)
 
-    if "chunk" in query and query["chunk"]:
-        result = chunk(events, query["chunk"])
+    if query.chunk:
+        result = chunk(events, query.chunk)
     else:
         result = {}
         result["eventcount"] = len(events)
@@ -63,10 +87,10 @@ def query(query: Union[Query, dict], ds, limit=-1, start=None, end=None):
 
 
 """
-
 CHUNKERS
-
+========
 """
+
 
 def chunk(events: List[Event], chunk_key: str):
     result = transforms.full_chunk(events, chunk_key)
@@ -83,14 +107,14 @@ def chunk(events: List[Event], chunk_key: str):
     result["duration"] = result["duration"].total_seconds()
     return result
 
-"""
 
+"""
 FILTERS
-
+=======
 """
 
 
-def filter_keyvals(tfilter, events, ds, limit=-1, start=None, end=None, exclude=False):
+def filter_keyvals(tfilter, events: List[Event], ds: Datastore, limit=-1, start: datetime=None, end: datetime=None, exclude: bool=False):
     if "key" not in tfilter:
         raise QueryException("filter_keyvals filter misses key field: {}".format(tfilter))
     elif "vals" not in tfilter:
@@ -117,6 +141,7 @@ def timeperiod_intersect(tfilter, events, ds, limit=-1, start=None, end=None):
         filterevents += bucket_transform(btransform, ds, limit, start, end)
     events = transforms.filter_period_intersect(events, filterevents)
     return events
+
 
 filters = {
     'exclude_keyvals': exclude_keyvals,
