932ddbef63997d3994e116da225fbdd14819fc26
Erik Bjäreholt <erik@bjareho.lt>
2017-06-20 13:11:23 +0200

    improved typing, made get_X_dir accept None if caller doesn't want a subdir

--- aw_core/dirs.py
+++ aw_core/dirs.py
@@ -1,38 +1,45 @@
 import os
 from functools import wraps
+from typing import Optional, Callable
 
 import appdirs
 
+GetDirFunc = Callable[[Optional[str]], str]
 
-def ensure_returned_path_exists(f):
+
+def ensure_path_exists(path: str) -> None:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def _ensure_returned_path_exists(f: GetDirFunc) -> GetDirFunc:
     @wraps(f)
-    def wrapper(*args, **kwargs) -> str:
-        path = f(*args, **kwargs)
-        if not os.path.exists(path):
-            os.makedirs(path)
+    def wrapper(subpath: Optional[str]) -> str:
+        path = f(subpath)
+        ensure_path_exists(path)
         return path
     return wrapper
 
 
-@ensure_returned_path_exists
-def get_data_dir(module_name: str) -> str:
-    user_data_dir = appdirs.user_data_dir("activitywatch")
-    return os.path.join(user_data_dir, module_name)
+@_ensure_returned_path_exists
+def get_data_dir(module_name: Optional[str]) -> str:
+    data_dir = appdirs.user_data_dir("activitywatch")
+    return os.path.join(data_dir, module_name) if module_name else data_dir
 
 
-@ensure_returned_path_exists
-def get_cache_dir(module_name: str) -> str:
-    user_cache_dir = appdirs.user_cache_dir("activitywatch")
-    return os.path.join(user_cache_dir, module_name)
+@_ensure_returned_path_exists
+def get_cache_dir(module_name: Optional[str]) -> str:
+    cache_dir = appdirs.user_cache_dir("activitywatch")
+    return os.path.join(cache_dir, module_name) if module_name else cache_dir
 
 
-@ensure_returned_path_exists
-def get_config_dir(module_name: str) -> str:
-    user_config_dir = appdirs.user_config_dir("activitywatch")
-    return os.path.join(user_config_dir, module_name)
+@_ensure_returned_path_exists
+def get_config_dir(module_name: Optional[str]) -> str:
+    config_dir = appdirs.user_config_dir("activitywatch")
+    return os.path.join(config_dir, module_name) if module_name else config_dir
 
 
-@ensure_returned_path_exists
-def get_log_dir(module_name: str) -> str:  # pragma: no cover
-    user_log_dir = appdirs.user_log_dir("activitywatch")
-    return os.path.join(user_log_dir, module_name)
+@_ensure_returned_path_exists
+def get_log_dir(module_name: Optional[str]) -> str:  # pragma: no cover
+    log_dir = appdirs.user_log_dir("activitywatch")
+    return os.path.join(log_dir, module_name) if module_name else log_dir
