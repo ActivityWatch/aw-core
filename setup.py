a3b2fa2ec1dec52176e1cdc3a8664942686be092
Erik Bjäreholt <erik@bjareho.lt>
2017-08-25 10:00:02 +0200

    switched to using requirements.txt

--- setup.py
+++ setup.py
@@ -16,17 +16,6 @@ with open(os.path.join(base_dir, "aw_core", "__about__.py")) as f:
 with open(os.path.join(base_dir, "README.md")) as f:
     long_description = f.read()
 
-if False:
-    with open(os.path.join(base_dir, "CHANGELOG.rst")) as f:
-        # Remove :issue:`ddd` tags that breaks the description rendering
-        changelog = re.sub(
-            r":issue:`(\d+)`",
-            r"`#\1 <https://github.com/pypa/pipfile/issues/\1>`__",
-            f.read(),
-        )
-        long_description = "\n".join([long_description, changelog])
-
-
 setup(name=about["__title__"],
       version=about["__version__"],
       description=about["__summary__"],
@@ -36,14 +25,6 @@ setup(name=about["__title__"],
       url=about["__uri__"],
       packages=set(["aw_core", "aw_datastore", "aw_datastore.storages"]),
       package_data={"aw_core": ['schemas/*.json']},
-      install_requires=[
-          'jsonschema',
-          'peewee>=2.8.5',
-          'strict-rfc3339',
-          'appdirs>=1.4.0',
-          'iso8601',
-          'python-json-logger>=0.1.5',
-          'takethetime>=0.3.0'],
       classifiers=[
           'Programming Language :: Python :: 3']
       )
