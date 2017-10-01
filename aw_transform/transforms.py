70af44c34969b3069adf690f6ca5c329fe871ddc
Johan Bjäreholt <johan@bjareho.lt>
2017-10-01 14:50:29 +0200

    Added merge_events_by_keys, fixed variables/functions with numbers and fixed strings

--- aw_transform/transforms.py
+++ aw_transform/transforms.py
@@ -265,3 +265,29 @@ def merge_queries(q1, q2):
     if "eventlist" in q1 and "eventlist" in q2:
         result["eventlist"] = q1["eventlist"] + q2["eventlist"]
     return result
+
+def merge_events_by_keys(events, keys):
+    # The result will be a list of events without timestamp since they are merged
+    # Call recursively until all keys are consumed
+    if len(keys) < 1:
+        return events
+    merged_events = {}
+    for event in events:
+        summed_key = ""
+        for key in keys:
+            if key in event.data:
+                summed_key = summed_key + "." + event["data"][key]
+        print(summed_key)
+        if summed_key not in merged_events:
+            merged_events[summed_key] = deepcopy(event)
+            merged_events[summed_key].data = {}
+            for key in keys:
+                if key in event.data:
+                    merged_events[summed_key].data[key] = event.data[key]
+        else:
+            merged_events[summed_key].duration += event.duration
+    print(merged_events)
+    result = []
+    for key in merged_events:
+        result.append(merged_events[key])
+    return result
