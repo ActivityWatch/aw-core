1069361407ce034670359cd4ffa42d79ec427858
Johan Bjäreholt <johan@bjareho.lt>
2017-11-19 16:17:35 +0100

    Single quote strings and no more meta variables

--- tests/test_query2.py
+++ tests/test_query2.py
@@ -20,6 +20,9 @@ def test_query2_test_token_parsing():
     (t, token), trash = _parse_token('"test"', ns)
     assert token == '"test"'
     assert t == String
+    (t, token), trash = _parse_token("'test'", ns)
+    assert token == "'test'"
+    assert t == String
     (t, token), trash = _parse_token("test0xDEADBEEF", ns)
     assert token == "test0xDEADBEEF"
     assert t == Variable
@@ -43,99 +46,89 @@ def test_query2_test_token_parsing():
     except QueryException:
         pass
 
-def test_query2_test_bogus_query():
+def test_query2_bogus_query():
+    qname="test"
+    qstartdate=datetime.now()
+    qenddate=qstartdate
     try: # Assign to non-variable
         example_query = "1=2"
-        result = query(example_query, None)
+        result = query(qname, example_query, qstartdate, qenddate, None)
         assert(False)
     except QueryException:
         pass
     try: # Unclosed function
-        query("a=unclosed_function(var1", None)
+        example_query = "a=unclosed_function(var1"
+        result = query(qname, example_query, qstartdate, qenddate, None)
         assert(False)
     except QueryException:
         pass
     try: # Two tokens in assignment
         example_query = "asd nop()=2"
-        result = query(example_query, None)
+        result = query(qname, example_query, qstartdate, qenddate, None)
         assert(False)
     except QueryException:
         pass
     try: # Unvlosed string
         example_query = 'asd="something is wrong with me'
-        result = query(example_query, None)
+        result = query(qname, example_query, qstartdate, qenddate, None)
         assert(False)
     except QueryException:
         pass
     try: # Two tokens in value
         example_query = "asd=asd1 asd2"
-        result = query(example_query, None)
+        result = query(qname, example_query, qstartdate, qenddate, None)
         assert(False)
     except QueryException:
         pass
 
-def test_query2_test_query_function_calling():
+def test_query2_query_function_calling():
+    qname="asd"
+    starttime=iso8601.parse_date("1970-01-01")
+    endtime=iso8601.parse_date("1970-01-02")
     try: # Function which doesn't exist
-        example_query = """NAME="asd";
-        STARTTIME="1970-01-01";
-        ENDTIME="1970-01-02";
-        RETURN=asd();"""
-        result = query(example_query, None)
+        example_query = "RETURN=asd();"
+        result = query(qname, example_query, starttime, endtime, None)
         assert False
     except QueryException as e:
         print(e)
     try: # Function which does exist with invalid arguments
-        example_query = """NAME="asd";
-        STARTTIME="1970-01-01";
-        ENDTIME="1970-01-02";
-        RETURN=nop(STARTTIME);"""
-        result = query(example_query, None)
+        example_query = "RETURN=nop(badarg);"
+        result = query(qname, example_query, starttime, endtime, None)
         assert False
     except QueryException as e:
         print(e)
-    try: # Function which does exist with invalid arguments
-        example_query = """NAME="asd";
-        STARTTIME="1970-01-01";
-        ENDTIME="1970-01-02";
-        RETURN=nop();"""
-        result = query(example_query, None)
-    except Exception:
-        assert False
+    # Function which does exist with valid arguments
+    example_query = "RETURN=nop();"
+    result = query(qname, example_query, starttime, endtime, None)
 
-def test_query2_test_return_value():
-    example_query = """
-    NAME="asd";
-    STARTTIME="2017";
-    ENDTIME="2017";
-    RETURN=1;
-    """
-    assert(query(example_query, None) == 1)
+def test_query2_return_value():
+    qname="asd"
+    starttime=iso8601.parse_date("1970-01-01")
+    endtime=iso8601.parse_date("1970-01-02")
+    example_query = "RETURN=1;"
+    result = query(qname, example_query, starttime, endtime, None)
+    assert(result == 1)
 
-    example_query = """
-    NAME="asd2";
-    STARTTIME="2017";
-    ENDTIME="2017";
-    RETURN="testing 123";
-    """
-    assert(query(example_query, None) == "testing 123")
+    example_query = "RETURN='testing 123'"
+    result = query(qname, example_query, starttime, endtime, None)
+    assert(result == "testing 123")
+
     # TODO: test dict/array
 
 @pytest.mark.parametrize("datastore", param_datastore_objects())
-def test_query2_test_query_functions(datastore):
+def test_query2_query_functions(datastore):
     """
     Just test calling all functions just to see something isn't completely broken
     In many cases the functions doesn't change the result at all, so it's not a test
     for testing the validity of the data the functions transform
     """
     bid = "test_bucket"
+    qname = "test"
+    cache = False
     starttime = iso8601.parse_date("1970")
     endtime = starttime + timedelta(hours=1)
     example_query = \
     """
-    NAME="test";
-    CACHE=FALSE;
-    STARTTIME="{}";
-    ENDTIME="{}";
     bid="{}";
     events=query_bucket(bid);
     events2=query_bucket(bid);
@@ -148,38 +141,36 @@ def test_query2_test_query_functions(datastore):
     events=sort_by_duration(events);
     asd=nop();
     RETURN=events;
-    """.format(starttime, endtime, bid)
+    """.format(bid)
     try:
         bucket = datastore.create_bucket(bucket_id=bid, type="test", client="test", hostname="test", name="asd")
         e1 = Event(data={"label": "test1"},
                    timestamp=starttime,
                    duration=timedelta(seconds=1))
         bucket.insert(e1)
-        result = query(example_query, datastore)
+        result = query(qname, example_query, starttime, endtime, datastore, cache)
         print(result)
         assert result[0].data["label"] == "test1"
     finally:
         datastore.delete_bucket(bid)
 
 @pytest.mark.parametrize("datastore", param_datastore_objects())
-def test_query2_test_basic_query(datastore):
+def test_query2_basic_query(datastore):
     name = "A label/name for a test bucket"
     bid1 = "bucket1"
     bid2 = "bucket2"
+    qname = "test_query_basic"
+    cache = True
     starttime = iso8601.parse_date("1970")
     endtime = starttime + timedelta(hours=1)
     example_query = \
     """
-    NAME="test_query_basic";
-    CACHE=TRUE;
-    STARTTIME="{}";
-    ENDTIME="{}";
     bid1="{}";
     bid2="{}";
     events=query_bucket(bid1);
     intersect_events=query_bucket(bid2);
     RETURN=filter_period_intersect(events, intersect_events);
-    """.format(starttime, endtime, bid1, bid2)
+    """.format(bid1, bid2)
     try:
         # Setup buckets
         bucket1 = datastore.create_bucket(bucket_id=bid1, type="test", client="test", hostname="test", name=name)
@@ -198,9 +189,9 @@ def test_query2_test_basic_query(datastore):
         bucket1.insert(e2)
         bucket2.insert(et)
         # Query
-        result = query(example_query, datastore)
+        result = query(qname, example_query, starttime, endtime, datastore, cache)
         # Query again for cache hit
-        result = query(example_query, datastore)
+        result = query(qname, example_query, starttime, endtime, datastore, cache)
         # Assert
         assert(len(result) == 1)
         assert(result[0]["data"]["label"] == "test1")
@@ -212,20 +203,18 @@ def test_query2_test_basic_query(datastore):
 def test_query2_test_merged_keys(datastore):
     name = "A label/name for a test bucket"
     bid1 = "bucket1"
+    qname = "test_query_merged_keys"
     starttime = iso8601.parse_date("2080")
     endtime = starttime + timedelta(hours=1)
+    cache = True
     example_query = \
     """
-    NAME="test_query_merged_keys";
-    CACHE=TRUE;
-    STARTTIME="{}";
-    ENDTIME="{}";
     bid1="{}";
     events=query_bucket(bid1);
     events=merge_events_by_keys(events, "label1", "label2");
     events=sort_by_duration(events);
     RETURN=events;
-    """.format(starttime, endtime, bid1)
+    """.format(bid1)
     try:
         # Setup buckets
         bucket1 = datastore.create_bucket(bucket_id=bid1, type="test", client="test", hostname="test", name=name)
@@ -243,9 +232,9 @@ def test_query2_test_merged_keys(datastore):
         bucket1.insert(e1)
         bucket1.insert(e2)
         # Query
-        result = query(example_query, datastore)
+        result = query(qname, example_query, starttime, endtime, datastore, cache)
         # Query again for cache miss (it's not year 2080 yet, I hope?)
-        result = query(example_query, datastore)
+        result = query(qname, example_query, starttime, endtime, datastore, cache)
         # Assert
         assert(len(result) == 2)
         assert(result[0]["data"]["label1"] == "test1")
