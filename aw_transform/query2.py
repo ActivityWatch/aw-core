6b37cbe0cfcbad94f5ca7e76cb316868a313e1d3
Johan Bjäreholt <johan@bjareho.lt>
2018-01-13 14:11:46 +0100

    Fixed bug when strings in function calls contains a )

--- aw_transform/query2.py
+++ aw_transform/query2.py
@@ -146,35 +146,43 @@ class Function(Token):
         return Function(name, args)
 
     def check(string: str):
-        i = 0
+        args_start_i = 0
         # Find opening bracket
         found = False
         for char in string:
             if char.isalpha() or char == "_":
-                i = i+1
-            elif i != 0 and char.isdigit():
-                i = i+1
+                args_start_i = args_start_i + 1
+            elif args_start_i != 0 and char.isdigit():
+                args_start_i = args_start_i + 1
             elif char == '(':
-                i = i+1
+                args_start_i = args_start_i + 1
                 found = True
                 break
             else:
                 break
         if not found:
             return None, string
-        # Find closing bracket
-        to_consume = 1
-        for char in string[i:]:
-            i += 1
-            if char == ')':
-                to_consume = to_consume - 1
-            elif char == '(':
-                to_consume = to_consume + 1
-            if to_consume == 0:
-                break
-        if to_consume != 0:
-            raise QueryException("Unclosed function")
-        return string[:i], string[i+1:]
+        function_str = string[:args_start_i]
+        # Parse until closing bracket
+        args_str = string[args_start_i:].strip()
+        while len(args_str) > 0 and args_str[0] != ")":
+            if len(args_str) <= 0:
+                raise QueryException("Unclosed function")
+            (arg_t, arg), args_str = _parse_token(args_str, {})
+            function_str += arg
+            args_str = args_str.strip()
+            if len(args_str) > 0 and args_str[0] == ",":
+                args_str = args_str[1:].strip()
+                function_str += ","
+        if len(args_str) == 0:
+            # We never consume the ), so if the loop ends and it's not the
+            # last char the function call is not closed
+            raise QueryException("Unclosed function call")
+        # consume last ) from leftover
+        leftover_str = args_str[1:]
+        # replace , at end of function string with )
+        function_str += ")"
+        return function_str, leftover_str
 
 class Dict(Token):
     def __init__(self, value: dict):
