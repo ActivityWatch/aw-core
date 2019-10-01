6ffe444f0b755a7cfc7b35bdbc026a27f5a2f6a3
Erik Bjäreholt <erik@bjareho.lt>
2019-10-01 13:36:54 +0200

    prefixed computed data fields with '$'

--- aw_transform/split_url_events.py
+++ aw_transform/split_url_events.py
@@ -1,31 +1,23 @@
 import logging
-from datetime import datetime, timedelta
-from typing import List, Dict, Optional, Any
-from copy import copy, deepcopy
-import operator
-from functools import reduce
-from collections import defaultdict
+from typing import List
+
+from urllib.parse import urlparse
 
 from aw_core.models import Event
-from aw_core import TimePeriod
 
 logger = logging.getLogger(__name__)
 
 
-from urllib.parse import urlparse
-
-def split_url_events(events):
+def split_url_events(events: List[Event]) -> List[Event]:
     for event in events:
         if "url" in event.data:
             url = event.data["url"]
             parsed_url = urlparse(url)
-            event.data["protocol"] = parsed_url.scheme
-            event.data["domain"] = parsed_url.netloc
-            if event.data["domain"][:4] == "www.":
-                event.data["domain"] = event.data["domain"][4:]
-            event.data["path"] = parsed_url.path
-            event.data["params"] = parsed_url.params
-            event.data["options"] = parsed_url.query
-            event.data["identifier"] = parsed_url.fragment
+            event.data["$protocol"] = parsed_url.scheme
+            event.data["$domain"] = parsed_url.netloc[4:] if parsed_url.netloc[:4] == "www." else parsed_url.netloc
+            event.data["$path"] = parsed_url.path
+            event.data["$params"] = parsed_url.params
+            event.data["$options"] = parsed_url.query
+            event.data["$identifier"] = parsed_url.fragment
             # TODO: Parse user, port etc aswell
     return events
