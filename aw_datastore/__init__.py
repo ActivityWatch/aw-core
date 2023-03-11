a798e26ebb4ca433625707060e480d9548587dc8
Erik Bjäreholt <erik@bjareho.lt>
2023-03-11 11:10:09 +0100

    fix: removed mongodb storage

--- aw_datastore/__init__.py
+++ aw_datastore/__init__.py
@@ -1,4 +1,3 @@
-import platform as _platform
 from typing import Callable, Dict
 
 from . import storages
@@ -7,28 +6,13 @@ from .migration import check_for_migration
 
 
 def get_storage_methods() -> Dict[str, Callable[..., storages.AbstractStorage]]:
-    from .storages import (
-        MemoryStorage,
-        MongoDBStorage,
-        PeeweeStorage,
-        SqliteStorage,
-    )
+    from .storages import MemoryStorage, PeeweeStorage, SqliteStorage
 
     methods: Dict[str, Callable[..., storages.AbstractStorage]] = {
         PeeweeStorage.sid: PeeweeStorage,
         MemoryStorage.sid: MemoryStorage,
         SqliteStorage.sid: SqliteStorage,
     }
-
-    # MongoDB is not supported on Windows or macOS
-    if _platform.system() == "Linux":  # pragma: no branch
-        try:
-            import pymongo  # noqa: F401
-
-            methods[MongoDBStorage.sid] = MongoDBStorage
-        except ImportError:  # pragma: no cover
-            pass
-
     return methods
 
 
