330b415a27af04f8a21fc864b33f4c80d326498e
Erik Bjäreholt <erik@bjareho.lt>
2023-03-11 11:10:09 +0100

    fix: removed mongodb storage

--- aw_datastore/storages/__init__.py
+++ aw_datastore/storages/__init__.py
@@ -4,14 +4,12 @@ logger: _logging.Logger = _logging.getLogger(__name__)
 
 from .abstract import AbstractStorage
 from .memory import MemoryStorage
-from .mongodb import MongoDBStorage
 from .peewee import PeeweeStorage
 from .sqlite import SqliteStorage
 
 __all__ = [
     "AbstractStorage",
     "MemoryStorage",
-    "MongoDBStorage",
     "PeeweeStorage",
     "SqliteStorage",
 ]
